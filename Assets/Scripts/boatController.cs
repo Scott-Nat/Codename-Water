8a119d2ada13ad44624dd7d1bd60901141e63c38
Liam Ossenfort <lao52@drexel.edu>
2020-10-15 20:17:11 -0400

    Initial Boat Controller

---
+++ Assets/Scripts/boatController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boatController : MonoBehaviour
+{
+    //visible Properties
+    public Transform Motor;
+    public float SteerPower = 500f;
+    public float Power = 5f;
+    public float MaxSpeed = 10f;
+    public float Drag = 0.1f;
+
+    //used Components
+    private Rigidbody Rigidbody;
+    protected Quaternion StartRotation;
+    protected Camera Camera;
+
+    //internal Properties
+    protected Vector3 CamVel;
+
+
+    public void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        StartRotation = Motor.localRotation;
+        Camera = Camera.main;
+    }
+
+    public static void ApplyForceToReachVelocity(Rigidbody rigidbody, Vector3 velocity, float force = 1, ForceMode mode = ForceMode.Force)
+    {
+        if (force == 0 || velocity.magnitude == 0)
+            return;
+
+        velocity = velocity + velocity.normalized * 0.2f * rigidbody.drag;
+
+        force = Mathf.Clamp(force, -rigidbody.mass / Time.fixedDeltaTime, rigidbody.mass / Time.fixedDeltaTime);
+
+        if (rigidbody.velocity.magnitude == 0)
+        {
+            rigidbody.AddForce(velocity * force, mode);
+        }
+        else
+        {
+            var velocityProjectedToTarget = (velocity.normalized * Vector3.Dot(velocity, rigidbody.velocity) / velocity.magnitude);
+            rigidbody.AddForce((velocity - velocityProjectedToTarget) * force, mode);
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        //default direction
+        var forceDirection = transform.forward;
+        var steer = 0;
+
+        //steer direction [-1,0,1]
+        if (Input.GetKey(KeyCode.A))
+            steer = 1;
+        if (Input.GetKey(KeyCode.D))
+            steer = -1;
+
+
+        //Rotational Force
+        Rigidbody.AddForceAtPosition(steer * transform.right * SteerPower / 135f, Motor.position);
+
+        //compute vectors
+        var forward = Vector3.Scale(new Vector3(1, 0, 1), transform.forward);
+        var backward = Vector3.Scale(new Vector3(-1, 0 , -1), transform.forward);
+        var targetVel = Vector3.zero;
+
+        //forward/backward power
+        if (Input.GetKey(KeyCode.W))
+            ApplyForceToReachVelocity(Rigidbody, forward * MaxSpeed, Power);
+            var movingForward = Vector3.Cross(transform.forward, Rigidbody.velocity).y < 0;
+            Rigidbody.velocity = Quaternion.AngleAxis(Vector3.SignedAngle(Rigidbody.velocity, (movingForward ? 1f : 0f) * transform.forward, Vector3.up) * Drag, Vector3.up) * Rigidbody.velocity;
+
+        if (Input.GetKey(KeyCode.S))
+            ApplyForceToReachVelocity(Rigidbody, backward * (MaxSpeed * 0.75f), Power);
+            Rigidbody.velocity = Quaternion.AngleAxis(Vector3.SignedAngle(Rigidbody.velocity, (movingForward ? 1f : 0f) * transform.forward, Vector3.up) * Drag, Vector3.down) * Rigidbody.velocity;
+        //ApplyForceToReachVelocity(Rigidbody, forward * -MaxSpeed, Power);
+
+
+        //moving forward
+        //var movingForward = Vector3.Cross(transform.forward, Rigidbody.velocity).y < 0;
+
+        //move in direction
+        //Rigidbody.velocity = Quaternion.AngleAxis(Vector3.SignedAngle(Rigidbody.velocity, (movingForward ? 1f : 0f) * transform.forward, Vector3.up) * Drag, Vector3.up) * Rigidbody.velocity;
+    }
+
+}
